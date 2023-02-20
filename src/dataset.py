984db1a0eb3f26adc23138f7175b1ba31c51d226
Jeong <jeon_su@dw-000148l.intra.dlr.de>
2023-02-20 18:34:46 +0100

    UPDATE | fine tuning codeBERT with MSR(Big-vul) dataset

---
+++ src/dataset.py
@@ -0,0 +1,37 @@
+import torch
+from torch.utils.data import Dataset
+
+def get_labels(df):
+    unique_values = set(df.tolist())
+
+    # Create a dictionary to map the unique values to integer indices
+    value_to_index = {value: index for index, value in enumerate(unique_values)}
+
+    # Create a list of one-hot encoded labels for each row of the DataFrame
+    labels = []
+    for value in df:
+        index = value_to_index[value]
+        one_hot_label = torch.zeros(len(unique_values))
+        one_hot_label[index] = 1
+        labels.append(one_hot_label)
+    return labels
+
+
+def get_texts(df):
+    # Preprocess the data as necessary (e.g., get the texts from the DataFrame)
+    texts = df.tolist()
+    return texts
+
+# Create a PyTorch dataset
+class vulDataset(Dataset):
+  def __init__(self, encodings, labels):
+    self.encodings = encodings
+    self.labels = labels
+
+  def __getitem__(self, idx):
+    item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+    item['labels'] = torch.tensor(self.labels[idx])
+    return item
+
+  def __len__(self):
+    return len(self.labels)
