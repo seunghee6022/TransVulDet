80263afe8219714e91fea27c875159b4df2b9de7
Jeong <jeon_su@dw-000148l.intra.dlr.de>
2023-04-20 01:11:31 +0200

    UPDATE | modified the code and added plots

--- src/dataset.py
+++ src/dataset.py
@@ -1,8 +1,8 @@
-import torch
-import pandas as pd
-from torch.utils.data import Dataset
 import pickle
+import torch
+from torch.utils.data import Dataset
 
+'''
 def get_labels(df):
     # load dict to map the unique values to integer indices
     with open("data preprocessing/preprocessed datasets/total_cwe_dict.txt", "rb") as myFile:
@@ -16,29 +16,30 @@ def get_labels(df):
     for value in df:
         index = total_cwe_dict[value]
         labels.append(index)
+    return labels
+'''
 
-    #for value in df: #one-hot-encoded label
-        # index = total_cwe_dict[value]
-        # one_hot_label = torch.zeros(len(labels_list))
-        # one_hot_label[index] = 1
-        # labels.append(one_hot_label)
+
+def get_labels(df, num_labels):
+    # load dict to map the unique values to integer indices
+ 
+    # Create a list of one-hot encoded labels for each row of the DataFrame
+    labels = []
+    
+    # one-hot-encoded
+    for index in df:
+        one_hot_label = torch.zeros(num_labels)
+        one_hot_label[index] = 1
+        labels.append(one_hot_label)
+      
     return labels
 
 
-def get_texts(df, model_name=None):
+def get_texts(df):
     # Preprocess the data as necessary (e.g., get the texts from the DataFrame)
     texts = df.tolist()
-    if model_name == "CodeT5" or model_name == "T5":
-        # Define the classification task
-        task = 'classification'
-        prefix = task + ':'
-        texts = [prefix + text for text in texts]
-        return texts
-    
-    elif model_name == "GPT2" or model_name == "CodeGPT2":
-        pass
-
     return texts
+
 
 # Create a PyTorch dataset
 class vulDataset(Dataset):
@@ -46,34 +47,13 @@ class vulDataset(Dataset):
     self.encodings = encodings
     self.labels = labels
 
+
   def __getitem__(self, idx):
+  
     item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-    item['labels'] = torch.tensor(self.labels[idx])
+    item['labels'] = torch.tensor(self.labels[idx]).to(torch.float32)
+    
     return item
 
   def __len__(self):
     return len(self.labels)
-
-
-
-
-
-class MyDataset1(Dataset):
-    def __init__(self, csv_file):
-        self.data = pd.read_csv(csv_file)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        code = self.data.iloc[idx, :-1].values.astype('float32')
-        label = self.data.iloc[idx, -1:].values.astype('int64')
-        return code, label
-
-
-csv_file_1 = 'path/to/csv1'
-dataset1 = MyDataset1(csv_file_1)
-dataloader1 = DataLoader(dataset1, batch_size=32, shuffle=True)
-
-
-
