7c79fe79b6ff89f754ea7cc371fd3a9a28edf458
Seunghee <seunghee6022@gmail.com>
2023-09-24 02:16:31 +0200

    update trainer to distinguish to calculate loss from hierarchical classifier/default classifier

--- src/trainer.py
+++ src/trainer.py
@@ -4,7 +4,7 @@ from torch.nn import BCEWithLogitsLoss
 class CustomTrainer(Trainer):
     def __init__(self, use_hierarchical_classifier=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.loss_fn = BCEWithLogitsLoss()  
+        self.loss_fn = BCEWithLogitsLoss()
         self.use_hierarchical_classifier = use_hierarchical_classifier
 
     # For multilabel classification, need to define the Custom Loss Function
@@ -12,17 +12,16 @@ class CustomTrainer(Trainer):
         print("inputs['labels']",inputs['labels'].shape)
         batch_size, num_labels = inputs['labels'].shape
         print("batch_size", batch_size, "num_labels",num_labels)
-        
-        model_num_labels = model.model.config.num_labels if self.use_hierarchical_classifier else model.config.num_labels
-        print(model_num_labels)
-        # Only reshape if the number of labels doesn't match the model's config
-        if num_labels != model_num_labels:
-            print(f"num_labels {num_labels}!= model_num_labels {model_num_labels}")
 
-        logits = model(inputs['input_ids'], attention_mask=inputs['attention_mask'])[0]
+        if self.use_hierarchical_classifier:
+            loss, logits = model(inputs['input_ids'], attention_mask=inputs['attention_mask'], labels=inputs['labels'])
+            print("logits, inputs['labels']", logits.shape,  inputs['labels'].shape)
+        
+        else:
+            logits = model(inputs['input_ids'], attention_mask=inputs['attention_mask'])
+            loss = self.loss_fn(logits.view(-1, num_labels), 
+                            inputs['labels'].float().view(-1, num_labels))
         print("logits shape: ", logits.shape)
         print("labels shape: ", inputs['labels'].shape)
-        loss = self.loss_fn(logits.view(-1, num_labels), 
-                        inputs['labels'].float().view(-1, num_labels))
-        
+        print("loss:", loss)
         return (loss, logits) if return_outputs else loss
