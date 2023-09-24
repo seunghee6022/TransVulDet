050ef5a5f0d903b71e71558cca3c4dec2daef7b6
Seunghee <seunghee6022@gmail.com>
2023-09-24 02:16:31 +0200

    update trainer to distinguish to calculate loss from hierarchical classifier/default classifier

--- src/classifier.py
+++ src/classifier.py
@@ -8,17 +8,23 @@ import networkx as nx
 
 
 class BertWithHierarchicalClassifier(nn.Module):
-    def __init__(self, config: BertConfig, input_dim, embedding_dim, graph):
+    def __init__(self, model_name, embedding_dim, uid_to_dimension, graph):
         super(BertWithHierarchicalClassifier, self).__init__()
-        self.model = BertModel(config)
-        self.graph = graph
-        self.input_dim = input_dim
+        self.model_name = model_name
+        self.model = BertModel.from_pretrained(self.model_name)
+        
+        self.input_dim = self.model.config.hidden_size
         self.embedding_dim = embedding_dim
-        
-        # Here, replace BERT's linear classifier with your hierarchical classifier
+        self.graph = graph
+
+        # Here, replace BERT's linear classifier with hierarchical classifier
         self.classifier = HierarchicalClassifier(self.input_dim, self.embedding_dim, self.graph)
-        
-    def forward(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None, inputs_embeds=None):
+
+        self.uid_to_dimension = uid_to_dimension
+        self._force_prediction_targets = True
+        self.loss_weights = torch.ones(embedding_dim)
+
+    def forward(self, input_ids, attention_mask=None, labels=None, token_type_ids=None, position_ids=None, head_mask=None, inputs_embeds=None):
         outputs = self.model(
             input_ids,
             attention_mask=attention_mask,
@@ -27,45 +33,33 @@ class BertWithHierarchicalClassifier(nn.Module):
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
         )
-        
-        # I'm assuming you'd like to use the [CLS] token representation as features for your hierarchical classifier
+        # use the [CLS] token representation as features for hierarchical classifier
         cls_output = outputs[1]
         logits = self.classifier(cls_output)
+        if labels is not None:
+            loss = self.loss(logits, labels)
+            return loss, logits
         
         return logits
-
-class HierarchicalClassifier(nn.Module):
-    def __init__(self, input_dim, embedding_dim, graph):
-        super(HierarchicalClassifier, self).__init__()
-        self.linear = nn.Linear(input_dim, embedding_dim)
-        self.graph = graph
-        self._force_prediction_targets = True
-        self._l2_regularization_coefficient = 5e-5
-        self.uid_to_dimension = {}
-        self.prediction_target_uids = None
-        self.topo_sorted_uids = None
-        self.loss_weights = torch.ones(embedding_dim)
-
-        # Initialize weights and biases to zero
-        nn.init.zeros_(self.linear.weight)
-        nn.init.zeros_(self.linear.bias)
-
-    def set_uid_to_dimension(self):
-        all_uids = nx.topological_sort(self.graph)
-        self.topo_sorted_uids = list(all_uids)
-        self.uid_to_dimension = {
-                uid: dimension for dimension, uid in enumerate(self.topo_sorted_uids)
-            }
     
-        return self.uid_to_dimension
+    def one_hot_labels_to_cweIDs_labels(self, ground_truth):
+            '''
+            ground_truth is one-hot-encoded
+            uid: cwe id
+            '''
+            # Reverse the dictionary
+            dim_to_uid = {v: k for k, v in self.uid_to_dimension.items()}
+
+            # Get the indices where value is 1 in each row of ground_truth and map to keys
+            # indices = [row.nonzero().item() for row in ground_truth]
+            indices = torch.argmax(ground_truth, dim=1).tolist()
+            uid_labels = [dim_to_uid[idx] for idx in indices]
+           
+            return uid_labels
     
-    # predict_embedded funtion
-    def forward(self, x):
-        return torch.sigmoid(self.linear(x))
-    
-    # uid_to_dimension --> dict: {uid: #_dim}
     def embed(self, labels):
         embedding = np.zeros((len(labels), len(self.uid_to_dimension)))
+        
         for i, label in enumerate(labels):
             if label == 10000:
                 embedding[i] = 1.0
@@ -73,16 +67,25 @@ class HierarchicalClassifier(nn.Module):
                 embedding[i, self.uid_to_dimension[label]] = 1.0
                 for ancestor in nx.ancestors(self.graph, label):
                     embedding[i, self.uid_to_dimension[ancestor]] = 1.0
-                   
-        return embedding
+         # Convert numpy array to torch tensor
+        embedding_tensor = torch.tensor(embedding, dtype=torch.float32)
+        return embedding_tensor
     
-    def loss(self, feature_batch, ground_truth, weight_batch=None, global_step=None):
-        # If weight_batch is not provided, use a tensor of ones with the same shape as feature_batch
-        if weight_batch is None:
-            weight_batch = torch.ones_like(feature_batch[:, 0])
+    def loss(self, logits, one_hot_targets, weight_batch=None, global_step=None):
+        '''
+        ground_truth should be cwe id values. Given ground_truth is one-hot-encoded so needed to be converted to cwe_id list
+        '''
+        
+        targets = self.one_hot_labels_to_cweIDs_labels(one_hot_targets)
 
-        loss_mask = np.zeros((len(ground_truth), len(self.uid_to_dimension)))
-        for i, label in enumerate(ground_truth):
+        # If weight_batch is not provided, use a tensor of ones with the same shape as logits
+        if weight_batch is None:
+            weight_batch = torch.ones_like(logits[:, 0])
+
+        loss_mask = np.zeros((len(targets), len(self.uid_to_dimension)))
+        loss_mask = torch.tensor(loss_mask, dtype=torch.float32)
+        
+        for i, label in enumerate(targets):
             # Loss mask
             loss_mask[i, self.uid_to_dimension[label]] = 1.0
 
@@ -102,8 +105,8 @@ class HierarchicalClassifier(nn.Module):
                 for successor in self.graph.successors(label):
                     loss_mask[i, self.uid_to_dimension[successor]] = 1.0
 
-        embedding = self.embed(ground_truth)
-        prediction = self.forward(feature_batch) # forward instead of predict_embedded funtion
+        embedding = self.embed(targets)
+        prediction = logits # forward instead of predict_embedded funtion
         
         # Clipping predictions for stability
         clipped_probs = torch.clamp(prediction, 1e-7, 1.0 - 1e-7)
@@ -113,11 +116,32 @@ class HierarchicalClassifier(nn.Module):
             embedding * torch.log(clipped_probs) +
             (1.0 - embedding) * torch.log(1.0 - clipped_probs)
         )
+        
         sum_per_batch_element = torch.sum(
             the_loss * loss_mask * self.loss_weights, dim=1
         )
-        # This is your L2 regularization term
-        l2_penalty = self.l2_regularization_coefficient * torch.sum(self.linear.weight ** 2)
+        
+        # This is L2 regularization term
+        l2_penalty = self.classifier.l2_penalty()
         total_loss = torch.mean(sum_per_batch_element * weight_batch) + l2_penalty
         
         return total_loss
+
+class HierarchicalClassifier(nn.Module):
+    def __init__(self, input_dim=786, embedding_dim=None, graph=None):
+        super(HierarchicalClassifier, self).__init__()
+        self.linear = nn.Linear(input_dim, embedding_dim)
+        self.sigmoid = nn.Sigmoid() # Sigmoid activation layer
+        self._l2_regularization_coefficient = 5e-5
+       
+        # Initialize weights and biases to zero
+        nn.init.zeros_(self.linear.weight)
+        nn.init.zeros_(self.linear.bias)
+        
+    def forward(self, x):
+        x = self.linear(x)  # Linear transformation
+        x = self.sigmoid(x)  # Apply sigmoid activation function
+        return x
+    
+    def l2_penalty(self):
+        return self._l2_regularization_coefficient * torch.sum(self.linear.weight ** 2)
