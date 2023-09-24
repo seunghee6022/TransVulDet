ed8960ecbeb4a800c266d978e0d96ce42139f4c9
Seunghee <seunghee6022@gmail.com>
2023-09-24 23:08:05 +0200

    add custom compute_metrics

--- main_deep_classifier.py
+++ main_deep_classifier.py
@@ -8,6 +8,7 @@ from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import TrainingArguments
 from transformers import AdamW, get_linear_schedule_with_warmup
 from transformers import BertModel, BertConfig
+from transformers.trainer_callback import EarlyStoppingCallback
 
 import matplotlib.pyplot as plt
 
@@ -17,11 +18,32 @@ from src.trainer import CustomTrainer
 from src.dataset import CodeDataset, split_dataframe
 from src.graph import create_graph_from_json, set_uid_to_dimension
 from src.classifier import BertWithHierarchicalClassifier
+from sklearn.metrics import accuracy_score, f1_score
+
+
+def accuracy(evalPrediction):
+    print("Inside ACCURACY", evalPrediction)
+    yPred = evalPrediction.predictions
+    yTrue = evalPrediction.label_ids
+    print(yPred)
+    print(yTrue)
+    return {'accuracy':(yPred == yTrue).mean()}
   
+def compute_metrics(p):
+    # print("Inside comepute funtion: self.p:",self.p)
+    print("%%%%%%%%%%%%%%%%INSIDE COMPUTE METRICS")
+    predictions, labels = p.predictions, p.label_ids
+    print(f"prediction:{type(predictions)}\nlabels:{type(labels)}")
+    predictions = np.argmax(predictions, axis=1)
+    acc = accuracy_score(labels, predictions)
+    f1 = f1_score(labels, predictions, average='weighted')
+        
+    return {"accuracy": acc, "f1_score": f1}
+
 if __name__ == "__main__":
     print(os.getcwd())
     # # Create graph from JSON
-    paths_file = 'data_preprocessing/preprocessed_datasets/graph_all_paths.json'
+    paths_file = 'data_preprocessing/preprocessed_datasets/debug_datasets/graph_all_paths.json'
     with open(paths_file, 'r') as f:
         paths_dict_data = json.load(f)
    
@@ -40,6 +62,9 @@ if __name__ == "__main__":
     embedding_dim = num_labels
     uid_to_dimension = set_uid_to_dimension(graph)
 
+    # Check if a GPU is available and use it, otherwise, use CPU
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     if not use_hierarchical_classifier:
         config = BertConfig.from_pretrained(model_name, num_labels=num_labels)
         model = BertForSequenceClassification.from_pretrained(model_name, config=config)
@@ -49,6 +74,9 @@ if __name__ == "__main__":
 
     tokenizer = BertTokenizer.from_pretrained(model_name)
     print(f"use_hierarchical_classifier:{use_hierarchical_classifier} --> model:{model}")
+
+    # Move your model to the selected device
+    model.to(device)
 
     # Freeze all parameters of the model
     # By setting the requires_grad attribute to False, you can freeze the parameters so they won't be updated during training
@@ -64,21 +92,23 @@ if __name__ == "__main__":
 
     # Define Dataset
     # Split the DataFrame dataset into tran/val/test datasets and Tokenize the "code" column of your DataFrame
-    dataset_name = 'MVD_2000'
-    df_path = f'data_preprocessing/preprocessed_datasets/{dataset_name}.csv'
-    max_length = 512
+    dataset_name = 'MVD_100'
+    df_path = f'data_preprocessing/preprocessed_datasets/debug_datasets/{dataset_name}.csv'
+    max_length = 256
     lr= 1e-4
-    num_epoch = 5
+    num_epoch = 1
 
     train_df, val_df, test_df = split_dataframe(df_path)
     
-    train_encodings = tokenizer(list(train_df["code"]), truncation=True, padding=True, max_length=max_length, return_tensors="pt")
-    val_encodings = tokenizer(list(val_df["code"]), truncation=True, padding=True, max_length=max_length, return_tensors="pt")
-    test_encodings = tokenizer(list(test_df["code"]), truncation=True, padding=True, max_length=max_length, return_tensors="pt")
+    train_encodings = tokenizer(list(train_df["code"]), truncation=True, padding=True, max_length=max_length, return_tensors="pt").to(device)
+    val_encodings = tokenizer(list(val_df["code"]), truncation=True, padding=True, max_length=max_length, return_tensors="pt").to(device)
+    test_encodings = tokenizer(list(test_df["code"]), truncation=True, padding=True, max_length=max_length, return_tensors="pt").to(device)
 
     train_labels = list(train_df["cwe_id"])
     val_labels = list(val_df["cwe_id"])
     test_labels = list(test_df["cwe_id"])
+
+    print(test_labels)
 
     print("uid_to_dimension\n",uid_to_dimension)
 
@@ -92,10 +122,12 @@ if __name__ == "__main__":
     optimizer = AdamW(model.parameters(), lr=lr)
     # scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=num_train_steps)
 
+    callbacks = [EarlyStoppingCallback(2,0.8)]
 
     training_args = TrainingArguments(
         per_device_train_batch_size=batch_size,
         num_train_epochs=num_epoch,
+        weight_decay=0.01,
         logging_dir='./logs',
         output_dir='./outputs',
         evaluation_strategy="steps",
@@ -104,14 +136,20 @@ if __name__ == "__main__":
         learning_rate=lr,
         remove_unused_columns=False,  # Important for our custom loss function
         disable_tqdm=False,
+        load_best_model_at_end = True,
+        metric_for_best_model = "accuracy",
+        greater_is_better = True,
     )
 
     trainer = CustomTrainer(
         use_hierarchical_classifier = use_hierarchical_classifier,
         model=model,
         args=training_args,
+        tokenizer=tokenizer,
         train_dataset=train_dataset,
         eval_dataset=val_dataset,
+        callbacks=callbacks,
+        compute_metrics=compute_metrics, 
         # lr_scheduler=scheduler,  # Our custom loss function
     )
 
@@ -127,9 +165,10 @@ if __name__ == "__main__":
 
     # Print the evaluation results
     print("Evaluation results:", eval_results)
-
+    print("trainer.state",trainer.state)
     # Access the loss and metrics from the trainer's history
-    train_losses = trainer.state.log_history["loss"]
+    print("trainer.state.log_history",trainer.state.log_history)
+    train_losses = trainer.state.log_history["eval_loss"]
     val_accs = trainer.state.log_history["eval_accuracy"]
     val_f1_scores = trainer.state.log_history["eval_f1_score"]
 
