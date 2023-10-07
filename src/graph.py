a4481cd8a891010245ab8d9bf1781066ae3402c4
Seunghee <seunghee6022@gmail.com>
2023-10-07 01:51:03 +0000

    add validation for non cwe id in hierarchy nodes

--- src/graph.py
+++ src/graph.py
@@ -6,122 +6,31 @@ import pandas as pd
 from collections import OrderedDict
 # import pygraphviz as pgv
 from networkx.drawing.nx_agraph import graphviz_layout
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import pandas as pd
+import os
 
 
-# Assume x, true_labels are given tensors
-# prediction = model(x)
-# loss = model.calculate_loss(prediction, true_labels)
+def validate_all_nodes_in_total_cwe_id_list(total_cwe_id_list_dir, node_path_dir):
+    cnt =0
+    # Sample DataFrame
+    total_cwe_id_list_df = pd.read_csv(total_cwe_id_list_dir)
+    print(total_cwe_id_list_df.head(3))
 
-# class Classifier(nn.Module):
-#     def __init__(self, input_dim, embedding_dim, graph):
-#         super(Classifier, self).__init__()
-#         self.linear = nn.Linear(input_dim, embedding_dim)
-#         self.graph = graph
-#         self._force_prediction_targets = True
-#         self._l2_regularization_coefficient = 5e-5
-#         self.uid_to_dimension = {}
-#         self.prediction_target_uids = None
-#         self.topo_sorted_uids = None
-#         # self.loss_weights = None
-#         self.loss_weights = torch.ones(embedding_dim)
+    # Sample JSON object
+    with open(node_path_dir, 'r') as f:
+         node_data = json.load(f)
+   
+    print(node_data.keys())
+    # Check and print values in the DataFrame that are not in the JSON keys
+    for value in total_cwe_id_list_df['0']:
+        if str(value) not in node_data.keys():
+            print(value)
+            cnt+=1
+    if cnt:
+        print(f"Total cnt for cwe id not in hierarchy tree nodes:{cnt}")
+    else:
+        print("All cwe ids are correctly included in the hierarchy tree nodes!")
 
-#         # Initialize weights and biases to zero
-#         nn.init.zeros_(self.linear.weight)
-#         nn.init.zeros_(self.linear.bias)
-
-#     def set_uid_to_dimension(self):
-#         all_uids = nx.topological_sort(self.graph)
-#         print("all_uids\n",all_uids)
-#         self.topo_sorted_uids = list(all_uids)
-#         print("topo_sorted_uids\n",self.topo_sorted_uids)
-#         self.uid_to_dimension = {
-#                 uid: dimension for dimension, uid in enumerate(self.topo_sorted_uids)
-#             }
-    
-#         return self.uid_to_dimension
-    
-#     def forward(self, x):
-#         return torch.sigmoid(self.linear(x))
-    
-#     def predict_class(self, x):
-#         return (self.forward(x) > 0.5).float()  # Threshold at 0.5
-    
-#     def predict_embedded(self, x):
-#         return self.forward(x)
-    
-#     # uid_to_dimension --> dict: {uid: #_dim}
-#     def embed(self, labels):
-#         embedding = np.zeros((len(labels), len(self.uid_to_dimension)))
-#         print(embedding.shape, embedding)
-#         for i, label in enumerate(labels):
-#             if label == 10000:
-#                 embedding[i] = 1.0
-#             else:
-#                 print(f"[{i}] - label:{label}, uid_to_dimension[label]:{self.uid_to_dimension[label]}")
-#                 embedding[i, self.uid_to_dimension[label]] = 1.0
-#                 for ancestor in nx.ancestors(self.graph, label):
-#                     print("ancestor",ancestor, "uid_to_dimension[ancestor]",self.uid_to_dimension[ancestor])
-#                     embedding[i, self.uid_to_dimension[ancestor]] = 1.0
-#                     print("embedding",embedding)
-#         return embedding
-    
-#     def loss(self, feature_batch, ground_truth, weight_batch=None, global_step=None):
-#         # If weight_batch is not provided, use a tensor of ones with the same shape as feature_batch
-#         if weight_batch is None:
-#             weight_batch = torch.ones_like(feature_batch[:, 0])
-
-#         loss_mask = np.zeros((len(ground_truth), len(self.uid_to_dimension)))
-#         for i, label in enumerate(ground_truth):
-#             # Loss mask
-#             loss_mask[i, self.uid_to_dimension[label]] = 1.0
-
-#             for ancestor in nx.ancestors(self.graph, label):
-#                 loss_mask[i, self.uid_to_dimension[ancestor]] = 1.0
-#                 for successor in self.graph.successors(ancestor):
-#                     loss_mask[i, self.uid_to_dimension[successor]] = 1.0
-#                     # This should also cover the node itself, but we do it anyway
-
-#             if not self._force_prediction_targets:
-#                 # Learn direct successors in order to "stop"
-#                 # prediction at these nodes.
-#                 # If MLNP is active, then this can be ignored.
-#                 # Because we never want to predict
-#                 # inner nodes, we interpret labels at
-#                 # inner nodes as imprecise labels.
-#                 for successor in self.graph.successors(label):
-#                     loss_mask[i, self.uid_to_dimension[successor]] = 1.0
-
-#         embedding = self.embed(ground_truth)
-#         print("embedding",embedding.shape, embedding)
-#         prediction = self.predict_embedded(feature_batch)
-#         print("prediction", prediction.shape, prediction)
-
-#         # Clipping predictions for stability
-#         clipped_probs = torch.clamp(prediction, 1e-7, 1.0 - 1e-7)
-#         print("clipped_probs", clipped_probs.shape, clipped_probs)
-        
-#         # Binary cross entropy loss calculation
-#         the_loss = -(
-#             embedding * torch.log(clipped_probs) +
-#             (1.0 - embedding) * torch.log(1.0 - clipped_probs)
-#         )
-#         print("the_loss", the_loss)
-#         sum_per_batch_element = torch.sum(
-#             the_loss * loss_mask * self.loss_weights, dim=1
-#         )
-#         print("sum_per_batch_element", sum_per_batch_element)
-#         # This is your L2 regularization term
-#         l2_penalty = self.l2_regularization_coefficient * torch.sum(self.linear.weight ** 2)
-#         print("l2_penalty", l2_penalty)
-#         print("torch.mean(sum_per_batch_element * weight_batch)", torch.mean(sum_per_batch_element * weight_batch))
-#         total_loss = torch.mean(sum_per_batch_element * weight_batch) + l2_penalty
-#         print("total_loss", total_loss) 
-#         return total_loss
-       
 
 def sort_dict(node_levels):
     # Sort dictionary by keys
@@ -191,7 +100,11 @@ def show_tree_graph(graph):
 
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    print(os.getcwd())
+    total_cwe_id_list_dir='data_preprocessing/preprocessed_datasets/total_cwe_id_list.csv'
+    node_path_dir='datasets_/graph_all_paths.json'
+    validate_all_nodes_in_total_cwe_id_list(total_cwe_id_list_dir, node_path_dir)
 #     # Create graph from JSON
 #     paths_file = 'graph_all_paths.json'
 #     with open(paths_file, 'r') as f:
